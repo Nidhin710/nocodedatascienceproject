a2f6828f30db5ac8d6a79e03f08f4396acbe1f89
Nidhin710 <nidhin@proaibots.com>
2024-07-05 01:18:49 +0530

    changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ if uploaded_file is not None:
                     time.sleep(5)
                 X = uploaded_dataset[selected_features]
                 y = uploaded_dataset[target_column]
-                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
                 
                 lr = LinearRegression()
                 lr.fit(X_train, y_train)
